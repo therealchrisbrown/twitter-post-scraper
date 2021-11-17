b696dafff7e91e8ba0241616d3343ee5a24a915a
therealchrisbrown <ch.braun1@gmx.de>
2021-11-17 12:45:06 +0100

    add connection to mongodb

--- twitter-post-scraper.py
+++ twitter-post-scraper.py
@@ -5,6 +5,17 @@ from tweepy import user
 import pandas as pd
 import time, os, fnmatch, shutil
 import csv
+import pymongo
+from pymongo import MongoClient
+
+### COMPANY
+COMPANY = config('COMPANY_NAME')
+
+
+# Connect mongoDB
+cluster = MongoClient("mongodb+srv://cbscraper:" + config('mongodb_pw') +"@cluster0.qet17.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster ["scraperDB"]
+collection_twitter = db["twitter"]
 
 
 auth = tw.OAuthHandler(config('consumer_key'),config('consumer_secret'))
@@ -19,6 +30,8 @@ try:
     tweets_list = [[tweet.created_at, tweet.id, tweet.text] for tweet in tweets]
     tweets_list_header = ['Datum', 'Tweet-Id', 'Tweet']
     tweets_df = pd.DataFrame(tweets_list, columns=tweets_list_header).sort_values(['Datum'])
+    tweets_df["Company"] = COMPANY
+    tweets_df["Label"] = ""
 
 except BaseException as e:
     print('failed on_status,',str(e))
@@ -35,6 +48,9 @@ if last_date_current == last_date_existing:
 else:
     new_tweets = tweets_df.loc[lambda tweets_df: tweets_df['Datum'] > last_date_existing]
     conc_df = new_tweets.to_csv('./results/BDPTwitter.csv', mode='a', header=False, index=False, sep=';')
+
+    new_data_mdb_dict = new_tweets.to_dict('records')
+    collection_twitter.insert_many(new_data_mdb_dict)
 
     print("Es wurden Tweets hinzugefügt")
 
