a56d35a73b180b95ad90f890e9767f6855610e3b
therealchrisbrown <ch.braun1@gmx.de>
2021-11-17 12:08:15 +0100

    Build loop for new Tweets, add df logic

--- twitter-post-scraper.py
+++ twitter-post-scraper.py
@@ -4,6 +4,7 @@ import tweepy as tw
 from tweepy import user
 import pandas as pd
 import time, os, fnmatch, shutil
+import csv
 
 
 auth = tw.OAuthHandler(config('consumer_key'),config('consumer_secret'))
@@ -16,14 +17,24 @@ count = 150
 try:
     tweets = tw.Cursor(api.user_timeline, id=username).items(count)
     tweets_list = [[tweet.created_at, tweet.id, tweet.text] for tweet in tweets]
-    tweets_df = pd.DataFrame(tweets_list)
+    tweets_list_header = ['Datum', 'Tweet-Id', 'Tweet']
+    tweets_df = pd.DataFrame(tweets_list, columns=tweets_list_header).sort_values(['Datum'])
 
 except BaseException as e:
     print('failed on_status,',str(e))
     time.sleep(3)
+    
+existing_tw_data = pd.read_csv('./results/BDPTwitter.csv', error_bad_lines=False, sep=";")
 
-#print(tweets_df)
-t = time.localtime()
-timestamp = time.strftime('%Y%m%d',t)
-FILE_NAME = (timestamp + "_BDPTwitter.csv")
-tweets_df.to_csv('./results/' + FILE_NAME,index=False, sep=';')
+last_date_current = pd.to_datetime(tweets_df['Datum']).iloc[-1]
+last_date_existing = pd.to_datetime(existing_tw_data['Datum']).iloc[-1]
+
+
+if last_date_current == last_date_existing:
+    print('Keine neuen Tweets')
+else:
+    new_tweets = tweets_df.loc[lambda tweets_df: tweets_df['Datum'] > last_date_existing]
+    conc_df = new_tweets.to_csv('./results/BDPTwitter.csv', mode='a', header=False, index=False, sep=';')
+
+    print("Es wurden Tweets hinzugefügt")
+
