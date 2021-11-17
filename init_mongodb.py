2018ee91b9a8ef55013275a6b145c304d975660d
therealchrisbrown <ch.braun1@gmx.de>
2021-11-17 12:45:35 +0100

    init for setting up mongodb

---
+++ init_mongodb.py
@@ -0,0 +1,25 @@
+import pymongo
+from pymongo import MongoClient
+from decouple import config
+import pandas as pd
+
+
+# Connect mongoDB
+cluster = MongoClient("mongodb+srv://cbscraper:" + config('mongodb_pw') +"@cluster0.qet17.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster ["scraperDB"]
+collection_news = db["news"]
+collection_twitter = db["twitter"]
+
+# Data Twitter
+TWITTER_RESULTS_PATH = r"/Users/christianbraun/Google Drive/Microserv_scraper/py_twitter-posts-scraper/results/BDPTwitter.csv"
+data_twitter = pd.read_csv(TWITTER_RESULTS_PATH, error_bad_lines=False, sep=';')
+data_twitter["Company"] = "BPD Immobilienentwicklung"
+data_twitter["Label"] = ""
+
+#data_twitter.reset_index(inplace=True)
+data_twitter_dict = data_twitter.to_dict('records')
+
+# Post Data to mongoDB
+collection_twitter.insert_many(data_twitter_dict)
+
+#print(data_twitter_dict)
