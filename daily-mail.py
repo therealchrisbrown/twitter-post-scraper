f75a4366f10e1c00f640ef5f32333ed5b5676069
therealchrisbrown <ch.braun1@gmx.de>
2021-11-24 14:23:43 +0100

    new changes in mailing process

--- daily-mail.py
+++ daily-mail.py
@@ -7,6 +7,7 @@ import smtplib
 import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.message import EmailMessage
 
 
 # MongoDB Auth & Connection
@@ -40,34 +41,66 @@ print(mail_data)
 
 username = config('gmail_user')
 password = config('gmail_password')
-mail_from = username
-mail_to = "ralf.blum@upchain.io"
-mail_subject = 'BPD Immobilien neue Aktion für dich'
+msg = EmailMessage()
+#mail_from = username
+msg['From'] = username
+#mail_to = ['christian.braun@upchain.io','ch.braun02@gmail.com']
+msg['To'] = ['jannis.holthusen@upchain.io']
+msg['Subject'] = 'BPD Immobilien neue Aktion für dich'
+
+class Main:
+    KUNDE = 'Jannis Firma'
+HTML_File = open('index.html', 'r')
+s =HTML_File.read().format(p=Main())
+
+msg.set_content(s, subtype='html')
+
+
+# <!DOCTYPE html>
+# <html>
+#     <body>
+#         <div style="background-color:#eee;padding:10px 20px;">
+#             <h2 style="font-family:Open Sans, 'Times New Roman', Times, serif;color#454349;">Daily Report</h2>
+#         </div>
+#         <div style="padding:20px 0px">
+#             <div style="height: 500px;width:400px">
+#                 <div style="text-align:left;">
+#                     <h3>Kunde: </h3>
+#                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A ducimus deleniti nemo quibusdam iste sint!</p>
+#                     <a href="#">Read more</a>
+#                 </div>
+#             </div>
+#         </div>
+#     </body>
+# </html>
+
+#mail_subject = 'BPD Immobilien neue Aktion für dich'
 #mail_body = ""
 
-html = """\
-<html>
-  <head>Für deinen Kunden 'BPD Immobilien' gibt folgende Alerts</head>
-  <body>
-    {0}
-  </body>
-</html>
-""".format(mail_data.to_html())
+# html = """\
+# <html>
+#   <head>Für deinen Kunden 'BPD Immobilien' gibt folgende Alerts</head>
+#   <body>
+#     {0}
+#   </body>
+# </html>
+# """.format(mail_data.to_html())
 
-part1 = MIMEText(html, 'html')
-mimemsg = MIMEMultipart()
-mimemsg['From']=mail_from
-mimemsg['To']=mail_to
-mimemsg['Subject']=mail_subject
-mimemsg.attach(part1)
+# part1 = MIMEText(html, 'html')
+# mimemsg = MIMEMultipart()
+# # mimemsg['From']=mail_from
+# # mimemsg['To']=mail_to
+# # mimemsg['Subject']=mail_subject
+# mimemsg.attach(part1)
 
 try:
-    connection = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    #connection.starttls()
-    connection.login(username,password)
-    connection.send_message(mimemsg)
-    connection.quit()
-    print("Email erfolgreich versendet :-)")
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:
+        connection.login(username,password)
+        connection.send_message(msg)
+        connection.quit()
+        print("Email erfolgreich versendet :-)")
+
+
 except BaseException as e:
     print('failed on_status,',str(e))
     time.sleep(3)
