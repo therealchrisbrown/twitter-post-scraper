681ed3c6fa4f334a2482dea250c2663c0c6c5765
therealchrisbrown <ch.braun1@gmx.de>
2021-12-01 16:37:16 +0100

    adding scheduler in code(2mins)

--- twitter-post-scraper.py
+++ twitter-post-scraper.py
@@ -6,62 +6,70 @@ import pandas as pd
 import time
 from pymongo import MongoClient
 import datetime
+import schedule
 
-### COMPANY
-COMPANY = config('COMPANY_NAME')
+def scraper():
+    ### COMPANY
+    COMPANY = config('COMPANY_NAME')
 
-# Connect mongoDB
-client = MongoClient(config('MONGODB_URI'))
-db = client[config('MONGODB_CLUSTER')]
-collection_twitter = db["twitter"]
+    # Connect mongoDB
+    client = MongoClient(config('MONGODB_URI'))
+    db = client[config('MONGODB_CLUSTER')]
+    collection_twitter = db["twitter"]
 
-# Twitter Auth & Config
-auth = tw.OAuthHandler(config('CONSUMER_KEY'),config('CONSUMER_SECRET'))
-auth.set_access_token(config('ACCESS_TOKEN'),config('ACCESS_SECRET'))
-api = tw.API(auth,wait_on_rate_limit=True)
-username = config('TWITTER_USER')
-count = 150
+    # Twitter Auth & Config
+    auth = tw.OAuthHandler(config('CONSUMER_KEY'),config('CONSUMER_SECRET'))
+    auth.set_access_token(config('ACCESS_TOKEN'),config('ACCESS_SECRET'))
+    api = tw.API(auth,wait_on_rate_limit=True)
+    username = config('TWITTER_USER')
+    count = 150
 
-### DATA
-try:
-    tweets = tw.Cursor(api.user_timeline, id=username).items(count)
-    tweets_list = []
-    for tweet in tweets:
-        created_at = tweet.created_at 
-        created_at = str(created_at)
-        tweet_dt = datetime.datetime.strptime(created_at, '%Y-%m-%d %H:%M:%S+00:00')
-        tweets_list.append(
-            {
-                'Datum': created_at,
-                'Tweet-Id': tweet.id,
-                'Tweet': tweet.text,
-                'Company': COMPANY,
-                'Source': "Twitter",
-                'Label': "",
-                'IsMailSent': "false"
-            }
-        )
-    tweets_df = pd.DataFrame(tweets_list).sort_values(by=['Datum'], ascending=True)
+    ### DATA
+    try:
+        tweets = tw.Cursor(api.user_timeline, id=username).items(count)
+        tweets_list = []
+        for tweet in tweets:
+            created_at = tweet.created_at 
+            created_at = str(created_at)
+            tweet_dt = datetime.datetime.strptime(created_at, '%Y-%m-%d %H:%M:%S+00:00')
+            tweets_list.append(
+                {
+                    'Datum': created_at,
+                    'Tweet-Id': tweet.id,
+                    'Tweet': tweet.text,
+                    'Company': COMPANY,
+                    'Source': "Twitter",
+                    'Label': "",
+                    'IsMailSent': "false"
+                }
+            )
+        tweets_df = pd.DataFrame(tweets_list).sort_values(by=['Datum'], ascending=True)
 
-except BaseException as e:
-    print('failed on_status,',str(e))
-    time.sleep(3)
-    
-### CHECK NEW DATA & ERGEBNIS
-existing_tw_data = pd.DataFrame(list(collection_twitter.find()))
-last_date_current = pd.to_datetime(tweets_df['Datum']).iloc[-1]
-last_date_existing = pd.to_datetime(existing_tw_data['Datum']).iloc[-1]
+    except BaseException as e:
+        print('failed on_status,',str(e))
+        time.sleep(3)
+        
+    ### CHECK NEW DATA & ERGEBNIS
+    existing_tw_data = pd.DataFrame(list(collection_twitter.find()))
+    last_date_current = pd.to_datetime(tweets_df['Datum']).iloc[-1]
+    last_date_existing = pd.to_datetime(existing_tw_data['Datum']).iloc[-1]
 
-if last_date_current == last_date_existing:
-    print("#### Keine neuen Tweets ####")
+    if last_date_current == last_date_existing:
+        print("#### Keine neuen Tweets ####")
 
-else:
-    new_tweets = tweets_df.loc[lambda tweets_df: pd.to_datetime(tweets_df['Datum']) > last_date_existing]
-    #conc_df = new_tweets.to_csv('./results/BDPTwitter.csv', mode='a', header=False, index=False, sep=';')
+    else:
+        new_tweets = tweets_df.loc[lambda tweets_df: pd.to_datetime(tweets_df['Datum']) > last_date_existing]
+        #conc_df = new_tweets.to_csv('./results/BDPTwitter.csv', mode='a', header=False, index=False, sep=';')
 
-    new_data_mdb_dict = new_tweets.to_dict('records')
-    collection_twitter.insert_many(new_data_mdb_dict)
+        new_data_mdb_dict = new_tweets.to_dict('records')
+        collection_twitter.insert_many(new_data_mdb_dict)
 
-    print("#### Es wurden Tweets hinzugefügt ####")
+        print("#### Es wurden Tweets hinzugefügt ####")
 
-print("Script ist gelaufen")
+    print("Script ist gelaufen")
+
+schedule.every(2).minutes.do(scraper)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
